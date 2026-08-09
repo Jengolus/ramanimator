0b6a3b816d78a2cd0a3d66f7a8226539e4b97803
Jengolus <jengolus@gmail.com>
2026-08-09 22:41:06 +0200

    Release 1.0.3

--- luascripts/ramanimator/raconfig.lua
+++ luascripts/ramanimator/raconfig.lua
@@ -5,7 +5,7 @@ To be considered immutable at runtime.
 
 local raconfig = {}
 
-raconfig.version = "1.0.2"
+raconfig.version = "1.0.3"
 raconfig.logLevel = 0
 raconfig.paletteWarningEmitted = false
 raconfig.featureGbPalettes = emu and emu.writePalette and emu.readPalette
